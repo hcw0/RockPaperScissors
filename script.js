af405fa44d3481238fdeb4673bbb331160331462
h <hchenwu02@gmail.com>
2022-05-30 04:27:47 -0400

    Add finished game

--- script.js
+++ script.js
@@ -1,70 +1,106 @@
+// 0 -> rock
+// 1 -> paper
+// 2 -> scissors
+
 let playerWins = 0
 let computerWins = 0;
 
 function computerPlay(){
-    let random = Math.floor(Math.random() * 3);
-    switch(random){
-        case 0:
-            return "Rock";
-            break;
-        case 1: 
-            return "Paper";
-            break;
-        case 2:
-            return "Scissors";
-            break;
+    let icon = document.querySelector("#pc-choice-icon");
+    let choice = Math.floor(Math.random() * 3);
+    if(choice == 0){
+        icon.className = "fa-regular fa-hand-back-fist";
+    } else if(choice == 1){
+        icon.className = "fa-regular fa-hand";
+    } else{
+        icon.className = "fa-regular fa-hand-scissors";
     }
+    console.log(choice);
+    return choice;
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if(playerSelection == computerSelection){
-        return "Tie";
-    } else if(playerSelection == "rock"){
-        if(computerSelection == "paper"){
-            computerWon();
-            return "You lose! Paper beats Rock";
+    if(!checkGameEnd()){
+        if(playerSelection == computerSelection){
+            roundSummary("Tie");
+        } else if(playerSelection == 0){
+            if(computerSelection == 1){
+                computerWon();
+                roundSummary("You lose! Paper beats Rock");
+            } else{
+                playerWon();
+                roundSummary("You won! Rock beats Scissors");
+            }
+        } else if(playerSelection == 1){
+            if(computerSelection == 0){
+                playerWon();
+                roundSummary("You won! Paper beats Rock");
+            } else{
+                computerWon();
+                roundSummary("You lost! Scissors beats Paper");
+            }
         } else{
-            playerWon();
-            return "You won! Rock beats Scissors";
-        }
-    } else if(playerSelection == "paper"){
-        if(computerSelection == "rock"){
-            playerWon();
-            return "You won! Paper beats Rock";
-        } else{
-            computerWon();
-            return "You lost! Scissors beats Paper";
-        }
-    } else{
-        if(computerSelection == "rock"){
-            computerWon();
-            return "You lost! Rock beats Scissors";
-        } else{
-            playerWon();
-            return "You won! Scissors beats Paper";
+            if(computerSelection == 1){
+                computerWon();
+                roundSummary("You won! Scissors beats Paper");
+            } else{
+                playerWon();
+                roundSummary("You lost! Rock beats Scissors");
+    
+            }
         }
     }
 }
 
-let game = (playerSelection) => {
-    for(let i = 0; i < 5; i++){
-        console.log(playRound(playerSelection, computerPlay()));
-    }
-    if(playerWins > computerWins){
-        console.log(`You won the majority of the games, with ${playerWins} wins against` + 
-        ` ${computerWins} of the computer's wins`);
-    } else if(playerWins < computerWins){
-        console.log(`You lost the majority of the games, with ${playerWins} wins against` + 
-        ` ${computerWins} of the computer's wins`);
-    } else{
-        console.log(`There was a tie as both you and the computer won ${playerWins} rounds`);
-    }
+
+
+const icons = document.querySelectorAll(".choices .fa-regular");
+console.log(icons);
+icons.forEach( icon => {
+    icon.addEventListener("click", () =>{
+        let id = icon.getAttribute("id"); 
+        if(id == "rock-selection"){
+            playRound(0, computerPlay());
+
+        } else if(id == "paper-selection"){
+            playRound(1, computerPlay());
+        } else{
+            playRound(2, computerPlay());
+        }
+    });
+})
+
+const userScore = document.querySelector(".user-score");
+const pcScore = document.querySelector(".computer-score");
+
+let playerWon = () => {
+    playerWins++;
+    userScore.textContent = playerWins;
+}
+let computerWon = () => {
+    computerWins++;
+    pcScore.textContent = computerWins;
 }
 
-let playerWon = () => playerWins++;
-let computerWon = () => computerWins++;
+let roundSummary = (string) => {
+    let p = document.querySelector("#round-summary");
+    p.textContent = string;
+    p.style.cssText = "text-align: center; font-size: 2em; margin-top: -2em;";
+    document.querySelector(".score-display").appendChild(p);
+}
 
+let checkGameEnd = () => {
+    if(playerWins == 5 || computerWins == 5){
+        let overlay = document.createElement("div");
+        overlay.style.cssText = "position: fixed; z-index: 9999; opacity: 0.5;" +
+            "background-color: black; width: 100%; min-height: 100vh; display: block;" +
+            "";
+        document.querySelector("body").insertBefore(overlay, document.querySelector(".header"));
 
-// game(prompt("Enter your choice(rock, paper, or scissors:)"));
+        let alert = document.createElement("div");
+
+        return true;
+    } else{
+        return false;
+    }
+}
